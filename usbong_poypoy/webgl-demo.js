fb8a3d450af1d79117d7654ea2958bf593d6ed44
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2024-04-19 15:10:50 +0800

    Add files via upload

---
+++ usbong_poypoy/webgl-demo.js
@@ -0,0 +1,46 @@
+/*
+  Copyright 2024 SYSON, MICHAEL B.
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You ' may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, ' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing ' permissions and limitations under the License.
+
+  @company: USBONG
+  @author: SYSON, MICHAEL B.
+  @date created: 20240419
+  @date updated: 20240419
+  @website address: http://www.usbong.ph
+
+  Start Page:
+  1) http://localhost/usbong_poypoy/index.html
+
+  Reminder:
+  1) install a web server, e.g. Apache Web Server via XAMPP 
+  
+  Reference:
+  1) https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL; last accessed: 20240419
+*/
+
+main();
+
+//
+// start here
+//
+function main() {
+  const canvas = document.querySelector("#glcanvas");
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    alert(
+      "Unable to initialize WebGL. Your browser or machine may not support it."
+    );
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
