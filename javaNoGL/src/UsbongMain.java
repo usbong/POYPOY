c41291c19c027943b41ea32918b172eff710c314
SYSON, MICHAEL B <masarapmabuhay@gmail.com>
2024-06-22 17:51:26 +0800

    Add files via upload

--- javaNoGL/src/UsbongMain.java
+++ javaNoGL/src/UsbongMain.java
@@ -3,9 +3,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- *     
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,16 +18,16 @@
  * @last updated: 20240622; from 20240621
  * @website: www.usbong.ph
  *
- */ 
+ */
 /*
  Additional Notes:
  1) compile on Windows Machine;
 	javac *.java
- 
+
  2) Execute
 	java UsbongMain
- 
-*/ 
+
+*/
 
 /*
 current error; 20240608; from 20240606
@@ -51,7 +51,7 @@ import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics; 
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseAdapter;
@@ -80,24 +80,28 @@ import java.util.Timer;
 import java.awt.Toolkit;
 import java.awt.Dimension;
 
+//added by Mike, 20240622
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 //TODO: -verify: in another computer with Java Virtual Machine
 
 public class UsbongMain {
-	
+
   //edited by Mike, 20240622
   //reference: Usbong Game Off 2023 Main
   //1000/60=16.66; 60 frames per second
-  //1000/30=33.33; 30 frames 
+  //1000/30=33.33; 30 frames
   //const fFramesPerSecondDefault=16.66;
-  //const fFramesPerSecondDefault=33.33;  
-  private static final int updateDelay = 16;//20;   
-  private static MyPanel mp;  
-    
+  //const fFramesPerSecondDefault=33.33;
+  private static final int updateDelay = 16;//20;
+  private static MyPanel mp;
+
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                createAndShowGUI(); 				
+                createAndShowGUI();
             }
         });
     }
@@ -105,56 +109,72 @@ public class UsbongMain {
     private static void createAndShowGUI() {
         //System.out.println("Created GUI on EDT? "+
         //SwingUtilities.isEventDispatchThread());
-        
+
 		JFrame f = new JFrame("Usbong Main Demo");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 		//edited by Mike, 20240622
         //f.setSize(250,250);
-		
+
 		//edited by Mike, 20240618
-	    //setSize(windowSize, windowSize);    
+	    //setSize(windowSize, windowSize);
 	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	    double dWidth = screenSize.getWidth();
         double dHeight = screenSize.getHeight();
-		
+
 	    int iWidth = (int) Math.round(dWidth);
 	    int iHeight = (int) Math.round(dHeight);
 
 	    System.out.println("width: "+iWidth);
 	    System.out.println("height: "+iHeight);
-			  
-	    f.setSize(iWidth, iHeight);	  	  	  
-	    f.setUndecorated(true); //removes close, minimize, maxime buttons in window
+
+      //edited by Mike, 20240622
+      //macOS still has menu row, etc.
+      //f.setSize(iWidth, iHeight);
+      f.setUndecorated(true); //removes close, minimize, maxime buttons in window
+
+      //Reference: https://stackoverflow.com/questions/1155838/how-can-i-do-full-screen-in-java-on-osx; last accessed: 20240622
+      //answered by: Michael Myers, 20090720T2105
+      GraphicsDevice gd =
+                  GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+      if (gd.isFullScreenSupported()) {
+          gd.setFullScreenWindow(f);
+      } else {
+          f.setSize(iWidth, iHeight);
+          f.setVisible(true);
+      }
+
 
 		//edited by Mike, 20240622
 		//f.add(new MyPanel());
 		mp = new MyPanel(f);
 		mp.setFocusable(true); //necessary to receive key presses
 		f.add(mp);
-		
-        f.setVisible(true);
-				
+
+    //removed by Mike, 20240622
+    //f.setVisible(true);
+
 		//timer.scheduleAtFixedRate(() -> mainRun(), 0, updateDelay, MILLISECONDS);
-		
+
 		TimerTask myTimerTask = new TimerTask(){
 		   public void run() {
 		     mp.update();
-			 
+
 			 mp.repaint();
 		   }
 		};
 
 		Timer myTimer = new Timer(true);
-		myTimer.scheduleAtFixedRate(myTimerTask, 0, updateDelay); //60 * 1000);		
-    } 
+		myTimer.scheduleAtFixedRate(myTimerTask, 0, updateDelay); //60 * 1000);
+    }
 }
 
 //note JPanel? JFrame? confusing at the start, without working sample code;
 class MyPanel extends JPanel {
 
     RedSquare redSquare;
-		
+
 	//added by Mike, 20240622
 	Dice myDice;
 	JFrame myJFrameInstance;
@@ -162,7 +182,7 @@ class MyPanel extends JPanel {
     public MyPanel(JFrame f) {
 		//added by Mike, 20240622
 		myJFrameInstance = f;
-		
+
 		redSquare  = new RedSquare();
 		myDice = new Dice();
 
@@ -179,29 +199,29 @@ class MyPanel extends JPanel {
                 moveSquare(e.getX(),e.getY());
             }
         });
-		
+
 		//added by Mike, 20240622
-		addKeyListener(new KeyListener() {    
-            public void keyPressed(KeyEvent key) {  
-                System.out.println("Key pressed."); 
+		addKeyListener(new KeyListener() {
+            public void keyPressed(KeyEvent key) {
+                System.out.println("Key pressed.");
 
 				if (key.getKeyCode() == KeyEvent.VK_ESCAPE){
-					System.out.println("ESC."); 
-					
+					System.out.println("ESC.");
+
 					//TODO: -add: open exit menu
 					myJFrameInstance.dispatchEvent(new WindowEvent(myJFrameInstance, WindowEvent.WINDOW_CLOSING));
-					
-				}				
-            }  
-			
-			public void keyReleased(KeyEvent key) {}    
-            public void keyTyped(KeyEvent key) {}			
-		});			
+
+				}
+            }
+
+			public void keyReleased(KeyEvent key) {}
+            public void keyTyped(KeyEvent key) {}
+		});
     }
 
     private void moveSquare(int x, int y){
 
-        // Current square state, stored as final variables 
+        // Current square state, stored as final variables
         // to avoid repeat invocations of the same methods.
         final int CURR_X = redSquare.getX();
         final int CURR_Y = redSquare.getY();
@@ -211,8 +231,8 @@ class MyPanel extends JPanel {
 
         if ((CURR_X!=x) || (CURR_Y!=y)) {
 
-            // The square is moving, repaint background 
-            // over the old square location. 
+            // The square is moving, repaint background
+            // over the old square location.
             repaint(CURR_X,CURR_Y,CURR_W+OFFSET,CURR_H+OFFSET);
 
             // Update coordinates.
@@ -220,8 +240,8 @@ class MyPanel extends JPanel {
             redSquare.setY(y);
 
             // Repaint the square at the new location.
-            repaint(redSquare.getX(), redSquare.getY(), 
-                    redSquare.getWidth()+OFFSET, 
+            repaint(redSquare.getX(), redSquare.getY(),
+                    redSquare.getWidth()+OFFSET,
                     redSquare.getHeight()+OFFSET);
         }
     }
@@ -229,24 +249,24 @@ class MyPanel extends JPanel {
     public Dimension getPreferredSize() {
         return new Dimension(250,200);
     }
-	
+
 	public void update() {
 		myDice.update();
-		
+
 		//OK
 		//System.out.println("!!!");
 	}
-    
+
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);       
+        super.paintComponent(g);
         //edited by Mike, 20240622
 		//g.drawString("This is my custom Panel!",10,20);
 		g.drawString("HALLO!",150,20);
 
         redSquare.paintSquare(g);
-		
+
 		myDice.draw(g);
-    }  
+    }
 }
 
 class RedSquare{
@@ -256,7 +276,7 @@ class RedSquare{
     private int width = 20;
     private int height = 20;
 
-    public void setX(int xPos){ 
+    public void setX(int xPos){
         this.xPos = xPos;
     }
 
@@ -274,7 +294,7 @@ class RedSquare{
 
     public int getWidth(){
         return width;
-    } 
+    }
 
     public int getHeight(){
         return height;
@@ -284,7 +304,7 @@ class RedSquare{
         g.setColor(Color.RED);
         g.fillRect(xPos,yPos,width,height);
         g.setColor(Color.BLACK);
-        g.drawRect(xPos,yPos,width,height);  
+        g.drawRect(xPos,yPos,width,height);
     }
 }
 
@@ -293,20 +313,20 @@ class Dice {
 	private int iFrameCount=0;
 	private int iFrameCountMax=4;
 	private int iFrameCountDelay=0;
-	private int iFrameCountDelayMax=20;	
-	
+	private int iFrameCountDelayMax=20;
+
 	private BufferedImage myBufferedImage;
-	
+
     public Dice() {
 	  try {
           //image = ImageIO.read(new File("D:/USBONG/res/count.png"));
 		  myBufferedImage = ImageIO.read(new File("../res/count.png"));
-      } catch (IOException ex) {			
-      }	  
-	}	
+      } catch (IOException ex) {
+      }
+	}
 
-/*	
-    public void setX(int xPos){ 
+/*
+    public void setX(int xPos){
         this.xPos = xPos;
     }
 
@@ -324,7 +344,7 @@ class Dice {
 
     public int getWidth(){
         return width;
-    } 
+    }
 
     public int getHeight(){
         return height;
@@ -337,25 +357,25 @@ class Dice {
 		else {
 			iFrameCount=(iFrameCount+1)%iFrameCountMax;
 			iFrameCountDelay=0;
-		}        
+		}
 	}
 
     public void draw(Graphics g){
 		//TODO: -verify: if clip still has to be cleared
 		Rectangle2D rect = new Rectangle2D.Float();
-		
+
 		//added by Mike, 20240621
 		//iFrameCount=2;
-		
+
 		//rect.setRect(0, 0, 128, 128);
 		//rect.setRect(iFrameCount*128, 0, 128, 128);
 		//note: clip rect has to move with object position
 		rect.setRect(iFrameCount*128-iFrameCount*128, 0, 128, 128);
 
 		Area myClipArea = new Area(rect);
-		g.setClip(myClipArea);	
-		//added by Mike, 20240621	
-		//g.drawImage(myBufferedImage, 0, 0, this);  
-		g.drawImage(myBufferedImage, 0-iFrameCount*128, 0, null);  		
+		g.setClip(myClipArea);
+		//added by Mike, 20240621
+		//g.drawImage(myBufferedImage, 0, 0, this);
+		g.drawImage(myBufferedImage, 0-iFrameCount*128, 0, null);
     }
 }
